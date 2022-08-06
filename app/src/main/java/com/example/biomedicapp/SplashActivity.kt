3ff3d472ac3a8bb91c2cd004a17b300e4bda9ec6
Andresbm_10 <andresbm_10@hotmail.com>
2022-08-06 17:34:55 -0500

    crear login, crear splash

--- app/src/main/java/com/example/biomedicapp/SplashActivity.kt
+++ app/src/main/java/com/example/biomedicapp/SplashActivity.kt
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {3
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
         finish()
     }
     override fun onDestroy() {
         super.onDestroy()
         finish()
     }
-}
+    override fun onRestart() {
+        super.onRestart()
+        finish()
+    }
+
+}         
